82b8b6daf69a16dfe630a58fa416ffad691ad73e
ratnov22 <antonratnov@yandex.ru>
2024-07-10 11:54:41 +0300

    add type to svgs props

--- src/svgs/LoaderSvg.tsx
+++ src/svgs/LoaderSvg.tsx
@@ -1,4 +1,6 @@
-export const LoaderSvg = (props) => (
+import { SVGProps } from 'react'
+
+export const LoaderSvg = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" {...props}>
     <circle
       cx={40}
